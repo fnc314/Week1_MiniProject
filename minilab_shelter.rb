8808c15163a2ddfe6be1cb72626314f26256837f
fnc314 <fnc314@gmail.com>
2014-01-11 21:02:32 -0800

    fixed shelter

--- minilab_shelter.rb
+++ minilab_shelter.rb
@@ -1,11 +1,43 @@
 class Shelter
 
-	attr_reader :name, 
+	attr_reader :name, :pets, :clients
 
-	def initialize(name)
+	def initialize(name, pets = [], clients = [])
 		@name = name
-		@pets = [] #of hashes
-	  @client = [] #of hashes
+		@pets = [] #intances of animal class
+	  @clients = [] #instances of client class
+	end
+
+	def take_in(animal)
+	  if animal.is_a?(Animal)
+	  	@pets << animal
+	  else
+	  	puts "#{animal} is not a valid animal."
+	  end
+	end
+
+	def add_client(client)
+		if client.is_a?(Client)
+			@clients << client
+		else
+			puts "I don't believe #{client} is a real person."
+		end
+	end
+
+	def list_clients
+		if @clients == []
+			puts "You don't have any clients you fool!"
+		else 
+			p @clients
+		end
+	end
+
+	def list_animals
+		if @pets == []
+			puts "PHEW!\n There aren't any animals in this wretched place."
+		else
+			p @pets
+		end
 	end
 
 end
